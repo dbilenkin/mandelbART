1838b174acbe8d5b2b2a13440f3f7029389288d1
dbilenkin <dbilenkin@gmail.com>
2019-02-19 00:42:01 -0500

    more pics

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Gallery from "./components/Gallery";
 import About from "./components/About";
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <div>
       <div className={`app-600`}>
         <Menu text>
